b7cb0ac24de0f8d5256fa972572b650e6cb61102
Tanishq49 <nottanishqbhandari@gmail.com>
2025-05-17 22:02:27 +0530

    Update call.js

--- call.js
+++ call.js
@@ -1,12 +1,4 @@
 const express = require('express')
 const app = express();
-const server = require('../api/server.js');
-const dotenv = require('dotenv');
 
-dotenv.config()
-
-
-server.listen(process.env.PORT,()=>{
-    console.log(`Server is running on http://localhost:${process.env.PORT}`)
-})
 module.exports = app;
